6e0c12b0ba90a24d6ca067db29a1b7192c7539a3
KubaNocon7890 <42960635+KonneckY@users.noreply.github.com>
2020-10-28 12:09:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -18,9 +18,9 @@
     <div class="main">
 
         <?php
-        $conn = new mysqli('localhost','root','','librarya');
+        $conn = new mysqli('remotemysql.com','Q5ce6IILxv','d8rRn6imdt','Q5ce6IILxv');
 
-        $result = $conn->query("SELECT * FROM `ksiazki`");
+        $result = $conn->query("SELECT id_autor_tytul, autor, tytul FROM lib_autor_tytul, lib_tytul, lib_autor WHERE lib_autor_tytul.id_autor=lib_autor.id_autor AND lib_autor_tytul.id_tytul=lib_tytul.id_tytul");
 
         echo "<table>";
         echo ("<tr>
